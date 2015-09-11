dfcf820de047fef0b336d47a09cb1e6de217c643
gudmundur-heimisson <gudmundur.heimisson@gmail.com>
2015-09-10 20:09:46 -0700

    Fix indentation to be more consistent.

--- public_html/cgi-bin/static/js/math/NumJS.js
+++ public_html/cgi-bin/static/js/math/NumJS.js
@@ -19,52 +19,52 @@ NumJS.prototype.map = function(x, fun) {
 };
 
 NumJS.prototype.sum = function(x) {
-	var sum = 0;
-	for (var i = 0; i < x.length; ++i) {
-		sum = sum + x[i];
-	}
-	return sum;
+    var sum = 0;
+    for (var i = 0; i < x.length; ++i) {
+        sum = sum + x[i];
+    }
+    return sum;
 };
 
 NumJS.prototype.erf = function(x) {
-	var t = 1/(1+0.5*Math.Abs(x));
-	var tau = t*Math.exp( Math.pow(x,2) 
-					- 1.26551223 
-					+ 1.00002368*t 
-					+ 0.37409196*Math.pow(t,2) 
-					+ 0.09679419*Math.pow(t,3) 
-					- 0.18628806*Math.pow(t,4) 
-					+ 0.27886807*Math.pow(t,5)
-					- 1.13520398*Math.pow(t,6)
-					+ 1.48851587*Math.pow(t,7)
-					- 0.82215223*Math.pow(t,8)
-					+ 0.17087277*Math.pow(t,9));
-	if (x >= 0) {
-		return 1-tau;
-	} else {
-		return tau-1;
-	}
+    var t = 1/(1+0.5*Math.Abs(x));
+    var tau = t*Math.exp( Math.pow(x,2) 
+                    - 1.26551223 
+                    + 1.00002368*t 
+                    + 0.37409196*Math.pow(t,2) 
+                    + 0.09679419*Math.pow(t,3) 
+                    - 0.18628806*Math.pow(t,4) 
+                    + 0.27886807*Math.pow(t,5)
+                    - 1.13520398*Math.pow(t,6)
+                    + 1.48851587*Math.pow(t,7)
+                    - 0.82215223*Math.pow(t,8)
+                    + 0.17087277*Math.pow(t,9));
+    if (x >= 0) {
+        return 1-tau;
+    } else {
+        return tau-1;
+    }
 };
 
 NumJS.prototype.mean = function(x) {
-	var n = x.length;
-	return this.sum(x)/n;
+    var n = x.length;
+    return this.sum(x)/n;
 };
 
 NumJS.prototype.variance = function(x) {
-	var mean = this.mean(x);
-	var variance = 0;
-	for (var i = 0; i < x.length; ++i) {
-		variance = variance + Math.pow(x[i],2);
-	}
-	variance = 1/(x.length-1) * variance;
-	variance = variance - mean;
-	return variance;
+    var mean = this.mean(x);
+    var variance = 0;
+    for (var i = 0; i < x.length; ++i) {
+        variance = variance + Math.pow(x[i],2);
+    }
+    variance = 1/(x.length-1) * variance;
+    variance = variance - mean;
+    return variance;
 };
 
 NumJS.prototype.std_dev = function(x) {
-	var variance = this.variance(x);
-	return Math.sqrt(variance);
+    var variance = this.variance(x);
+    return Math.sqrt(variance);
 };
 
 NumJS = new NumJS();
--- public_html/cgi-bin/static/js/math/Stats.js
+++ public_html/cgi-bin/static/js/math/Stats.js
@@ -11,93 +11,93 @@ Stats.prototype.rv_uniform = function (lower, upper, n) {
 };
 
 Stats.prototype.pdf_uniform = function(x, lower, upper) {
-	if (x >= lower && x <= upper) {
-		return 1/(upper-lower);
-	}
-	else {
-		return 0;
-	}
+    if (x >= lower && x <= upper) {
+        return 1/(upper-lower);
+    }
+    else {
+        return 0;
+    }
 };
 
 Stats.prototype.cdf_uniform = function(x, lower, upper) {
-	if (x >= lower && x <= upper) {
-		return (x-lower)/(upper-lower);
-	} else if (x < lower) {
-		return 0;
-	} else if (x > upper) {
-		return 1;
-	}
+    if (x >= lower && x <= upper) {
+        return (x-lower)/(upper-lower);
+    } else if (x < lower) {
+        return 0;
+    } else if (x > upper) {
+        return 1;
+    }
 };
 
 Stats.prototype.rv_normal = function(mu, sigma, n) {
-	var samples = new Array(n);
-	var U = this.rv_uniform(0, 1, n);
-	var V = this.rv_uniform(0, 1, n);
-	for (var i = 0; i < n; i++) {
-		samples[i] = mu + sigma * Math.sqrt(-2*Math.log(U[i])) * Math.cos(2*Math.PI*V[i]);
-	}
-	return samples;
+    var samples = new Array(n);
+    var U = this.rv_uniform(0, 1, n);
+    var V = this.rv_uniform(0, 1, n);
+    for (var i = 0; i < n; i++) {
+        samples[i] = mu + sigma * Math.sqrt(-2*Math.log(U[i])) * Math.cos(2*Math.PI*V[i]);
+    }
+    return samples;
 };
 
 Stats.prototype.pdf_normal = function(x, mu, sigma) {
-	return 1/(sigma * Math.sqrt(2 * Math.PI)) * Math.exp(-Math.pow(x-mu, 2)/(2 * Math.pow(sigma, 2)));
+    return 1/(sigma * Math.sqrt(2 * Math.PI)) * Math.exp(-Math.pow(x-mu, 2)/(2 * Math.pow(sigma, 2)));
 };
 
 Stats.prototype.cdf_normal = function(x, mu, sigma) {
-	return (1/2) * ( 1 + NumJS.erf( (x-mu)/(sigma*Math.sqrt(2)) ) );
+    return (1/2) * ( 1 + NumJS.erf( (x-mu)/(sigma*Math.sqrt(2)) ) );
 };
 
 Stats.prototype.rv_exponential = function(lambda, n) {
-	var samples = new Array(n);
-	var U = this.rv_uniform(0,1, n);
-	for (var i = 0; i < n; i++) {
-		samples[i] = -Math.log(U[i])/lambda;
-	}
-	return samples;
+    var samples = new Array(n);
+    var U = this.rv_uniform(0,1, n);
+    for (var i = 0; i < n; i++) {
+        samples[i] = -Math.log(U[i])/lambda;
+    }
+    return samples;
 };
 
 Stats.prototype.pdf_exponential = function(x, lambda) {
-	if (x < 0) {
-		return 0;
-	}
-	return lambda*Math.exp(-lambda*x);
+    if (x < 0) {
+        return 0;
+    }
+    return lambda*Math.exp(-lambda*x);
 };
 
 Stats.prototype.cdf_exponential = function(x, lambda) {
-	return 1-Math.exp(-lambda*x);
+    return 1-Math.exp(-lambda*x);
 };
 
 Stats.prototype.KernelDensityEstimator = function(samples) {
-	this.samples = samples;
-	var stddev = NumJS.std_dev(samples);
-	var n = this.samples.length;
-	this.normal_h = 1.06 * stddev * Math.pow(n, -1/5);
-	this.h = this.normal_h;
-	this.Kernel = this.NormalKernel;
+    this.samples = samples;
+    var stddev = NumJS.std_dev(samples);
+    var n = this.samples.length;
+    this.normal_h = 1.06 * stddev * Math.pow(n, -1/5);
+    this.h = this.normal_h;
+    this.Kernel = this.NormalKernel;
 };
 
 Stats.prototype.KernelDensityEstimator.prototype.NormalKernel = function(x) {
-	return Stats.pdf_normal(x, 0, 1);
+    return Stats.pdf_normal(x, 0, 1);
 };
 
 Stats.prototype.KernelDensityEstimator.prototype.RectangularKernel = function(x) {
-	return Stats.pdf_uniform(x, -1, 1);
+    return Stats.pdf_uniform(x, -1, 1);
 };
 
 Stats.prototype.KernelDensityEstimator.prototype.EpanechnikovKernel = function(x) {
-	return (3/2) * (1-Math.pow(x,2)) * Stats.pdf_uniform(x, -1, 1);
+    return (3/2) * (1-Math.pow(x,2)) * Stats.pdf_uniform(x, -1, 1);
 };
 
 Stats.prototype.KernelDensityEstimator.prototype.density = function(x) {
-	var kde = new Array(x.length);
-	for (var j = 0; j < x.length; ++j) {
-		kde[j] = 0;
-		for (var i = 0; i < this.samples.length; ++i) {
-			kde[j] = kde[j] + (1/this.h) * this.Kernel( (x[j] - this.samples[i]) / this.h );
-		}
-		kde[j] = kde[j]/this.samples.length;
-	}
-	return kde;
+    var kde = new Array(x.length);
+    for (var j = 0; j < x.length; ++j) {
+        kde[j] = 0;
+        for (var i = 0; i < this.samples.length; ++i) {
+            kde[j] = kde[j] + (1/this.h) * this.Kernel( (x[j] - this.samples[i]) / this.h );
+        }
+        kde[j] = kde[j]/this.samples.length;
+    }
+    return kde;
 };
 
 var Stats = new Stats();
