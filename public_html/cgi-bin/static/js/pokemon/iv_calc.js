5537a2a7af99c1249b5a05e8d5b0a8dec0e5728a
gudmundur-heimisson <gudmundur.heimisson@gmail.com>
2015-09-29 11:54:14 -0700

    Start refactor of IV calculation to add efficiency

--- public_html/cgi-bin/static/js/pokemon/iv_calc.js
+++ public_html/cgi-bin/static/js/pokemon/iv_calc.js
@@ -32,6 +32,34 @@ function Pokemon(level, baseStats, ivs, evs, nature) {
     }
 }
 
+function estimateIV(level, base, s, ev, n, isHP) {
+    var ivs = new Array(6);
+    var c = isHP ? level + 10 : 5;
+    var b = 2*base;
+    var e = Math.floor(ev/4);
+    var est = function(e1, e2) {return (100.0/level)*((s+e2)/n + e1 - c) - b - e;}
+    if (n == 1.0) {
+        var lower = est(0, 0);
+        var upper = est(0.99, 0);
+    } else {
+        var lower = est(0, 0);
+        var upper = est(0.99, 0.9);
+    }
+    var l = Math.max(Math.ceil(lower), 0);
+    var u = Math.min(Math.floor(upper), 31);
+    if (!(l>=0 && u<=31)) {
+        ivs.push(NaN);
+    } else {
+        for (var j=0; j<=u-l; ++j) {
+            var iv = l+j
+            if (s == computeStats(iv, base, ev, level, n, isHP)) {
+                ivs.push(iv);
+            }
+        }
+    }
+    return ivs;
+}
+
 function estimateIVs(level, baseStats, evs, nature, stats) {
     var natureMatrix = natureMatrices[nature];
     var ivs = new Array(6);
@@ -41,29 +69,32 @@ function estimateIVs(level, baseStats, evs, nature, stats) {
             var b = 2*baseStats[i];
             var e = Math.floor(evs[i]/4);
             var n = natureMatrix[i];
-            var c = i==0 ? 10+level:5;
-            var est = function(e1, e2) {return (100.0/level)*((s+e2)/n + e1 - c) - b - e;}
-            if (n == 1.0) {
-                var lower = est(0, 0);
-                var upper = est(0.99, 0);
-            } else {
-                var lower = est(0, 0);
-                var upper = est(0.99, 0.9);
-            }
-            var l = Math.max(Math.ceil(lower), 0);
-            var u = Math.min(Math.floor(upper), 31);
-            ivs[i] = new Array();
-            if (!(l>=0 && u<=31)) {
-                ivs[i].push(NaN);
-            } else {
-                for (var j=0; j<=u-l; ++j) {
-                    var iv = l+j
-                    if (s == computeStats(iv, baseStats[i], evs[i], level, n, isHP=i==0)) {
-                        ivs[i].push(iv);
-                    }
-                }
-            }
+            ivs.push(estimateIV(level, baseStats[i], stats[i], evs[i], natureMatrix[i], isHP=i==0));
         }
     }
+            // var c = i==0 ? 10+level:5;
+    //         var est = function(e1, e2) {return (100.0/level)*((s+e2)/n + e1 - c) - b - e;}
+    //         if (n == 1.0) {
+    //             var lower = est(0, 0);
+    //             var upper = est(0.99, 0);
+    //         } else {
+    //             var lower = est(0, 0);
+    //             var upper = est(0.99, 0.9);
+    //         }
+    //         var l = Math.max(Math.ceil(lower), 0);
+    //         var u = Math.min(Math.floor(upper), 31);
+    //         ivs[i] = new Array();
+    //         if (!(l>=0 && u<=31)) {
+    //             ivs[i].push(NaN);
+    //         } else {
+    //             for (var j=0; j<=u-l; ++j) {
+    //                 var iv = l+j
+    //                 if (s == computeStats(iv, baseStats[i], evs[i], level, n, isHP=i==0)) {
+    //                     ivs[i].push(iv);
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
     return ivs;
 }
