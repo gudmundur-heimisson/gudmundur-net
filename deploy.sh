ecf827e08f468f3a172d7b608b475ef966d4c874
Gudmundur Heimisson <gudmundur.heimisson@gmail.com>
2020-07-18 21:11:47 -0700

    Use a layout html file

--- deploy.sh
+++ deploy.sh
@@ -7,3 +7,6 @@
   --exclude=README.md \
   --exclude=server.go \
   ./ root@165.227.4.101:/usr/share/nginx/gudmundur.net/
+
+/usr/bin/rsync -trz \
+  ./nginx.conf root@165.227.4.101:/etc/nginx/sites-available/gudmundur.net
--- server.go
+++ server.go
@@ -5,16 +5,85 @@ package main
 import (
 	"log"
 	"net/http"
+	"strings"
+	"text/template"
+	// "net/url"
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path"
 )
 
-func main() {
+var pages = make(map[string]string)
+
+// Initialize page contents from layout template
+func init() {
+	layoutBytes, err := ioutil.ReadFile("./layout.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	layout := template.New("layout")
+	layout = template.Must(layout.Parse(string(layoutBytes)))
+	if err != nil {
+		log.Fatal(err)
+	}
+	var buf bytes.Buffer
+	fileInfos, err := ioutil.ReadDir("./templates")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, fileInfo := range fileInfos {
+		filePath := path.Join("./templates", fileInfo.Name())
+		fileContents, err := ioutil.ReadFile(filePath)
+		if err != nil {
+			log.Fatal(err)
+		}
+		layout.Execute(&buf, string(fileContents))
+		pageName := strings.TrimSuffix(fileInfo.Name(), ".html")
+		pages[pageName] = buf.String()
+		buf.Reset()
+	}
+}
+
+func handle404(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, pages["404"])
+	} else {
+		fmt.Fprintf(w, pages["index"])
+	}
+}
+
+func serve() {
+	generateTemplates()
 	css_fs := http.FileServer(http.Dir("./css/"))
 	js_fs := http.FileServer(http.Dir("./js/"))
-	static_fs := http.FileServer(http.Dir("./static/"))
 	favicon_fs := http.FileServer(http.Dir("./favicon/"))
 	http.Handle("/css/", http.StripPrefix("/css/", css_fs))
 	http.Handle("/js/", http.StripPrefix("/js/", js_fs))
 	http.Handle("/favicon/", http.StripPrefix("/favicon/", favicon_fs))
-	http.Handle("/", static_fs)
+	for page, _ := range pages {
+		http.HandleFunc("/"+page, func(w http.ResponseWriter, r *http.Request) {
+			http.ServeFile(w, r, path.Join("pages", page))
+		})
+	}
+	http.HandleFunc("/", handle404)
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func generateTemplates() {
+	dirname := "pages"
+	os.Mkdir(dirname, 0755)
+	for page, contents := range pages {
+		filename := path.Join(dirname, page)
+		// filename += ".html"
+		fmt.Println(filename)
+		ioutil.WriteFile(filename, []byte(contents), 0655)
+	}
+}
+
+func main() {
+	// generateTemplates()
+	serve()
 }
